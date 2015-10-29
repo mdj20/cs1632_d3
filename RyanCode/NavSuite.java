39b9f8b0b88d5aa7430010c3f453e11b58c19e7e
Aytros <Aytros@users.noreply.github.com>
2015-10-29 18:08:43 -0400

    Update NavSuite.java
    add user stories and scenarios

--- RyanCode/NavSuite.java
+++ RyanCode/NavSuite.java
@@ -11,6 +11,12 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 
+/*
+As a User
+I would like to have a navigation bar
+So that I may go to different areas of the website
+*/
+
 public class NavSuite {
 	private WebDriver driver;
 	private String baseUrl;
@@ -24,6 +30,11 @@ public class NavSuite {
 	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
+	/*
+	Given: Logged into a valid account
+	When: I look at the navigation bar
+	Then: I will see a link for my user profile
+	*/
 	@Test
 	public void testProfile() throws Exception {
 		driver.get(baseUrl + "/");
@@ -37,18 +48,33 @@ public class NavSuite {
 	    assertEquals("Aytros's Profile", driver.findElement(By.cssSelector("strong")).getText());
 	}
 
+	/*
+	Given: a loaded site
+	When: I view the navigation bar
+	Then: I will see a link to currently running games
+	*/
 	@Test
 	public void testGames() throws Exception {
 	  driver.get(baseUrl + "/");
 	  assertEquals("Games", driver.findElement(By.linkText("Games")).getText());
 	}
 	
+	/*
+	Given: a loaded site
+	When: I view the navigation bar
+	Then: I will see a link to view my character sheets
+	*/
 	@Test
 	public void testSheets() throws Exception {
 		driver.get(baseUrl + "/");
 		assertEquals("Sheets", driver.findElement(By.linkText("Sheets")).getText());
 	}
 	
+	/*
+	Given: a loaded site
+	When: I view the navigation bar
+	Then: I will see a link for the maps that I have created
+	*/
 	@Test
 	public void testMaps() throws Exception {
 		driver.get(baseUrl + "/");
@@ -61,6 +87,11 @@ public class NavSuite {
 	    assertEquals("Maps", driver.findElement(By.linkText("Maps")).getText());
 	}
 	
+	/*
+	Given: a loaded site
+	When: The navigation bar is viewed
+	Then: I will see a link to an online store
+	*/
 	@Test
 	public void testShop() throws Exception {
 		driver.get(baseUrl + "/");
@@ -68,6 +99,11 @@ public class NavSuite {
 	    assertEquals("Myth-Weavers Shop", driver.findElement(By.linkText("Myth-Weavers Shop")).getText());
 	}
 	
+	/*
+	Given: a loaded site
+	When: I view the navigation bar
+	Then: I will see a link to a wiki page
+	*/
 	@Test
 	public void testWiki() throws Exception {
 		driver.get(baseUrl + "/");
