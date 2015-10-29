7b574d76770ed2bbe4cf1b9d7caa0ccd2072a641
mdj20 <mdj20@pitt.edu>
2015-10-29 14:02:39 -0400

    All Matt's test's working
    all my test's are working

--- mattCode/exportedJunitTests/testSaveMsgju.java
+++ mattCode/exportedJunitTests/testSaveMsgju.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Testmsgju {
+public class TestSaveMsgju {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -23,7 +23,7 @@ public class Testmsgju {
   }
 
   @Test
-  public void testMsgju() throws Exception {
+  public void testSaveMsgju() throws Exception {
     driver.get(baseUrl + "/");
     driver.findElement(By.linkText("Log In")).click();
     driver.findElement(By.id("navbar_username")).clear();
@@ -36,18 +36,20 @@ public class Testmsgju {
     driver.findElement(By.linkText("Send Message")).click();
     driver.findElement(By.linkText("Send a private message to 1n50mniac")).click();
     driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys("test");
+    driver.findElement(By.name("title")).sendKeys("testing123");
     driver.findElement(By.id("vB_Editor_001_textarea")).clear();
     driver.findElement(By.id("vB_Editor_001_textarea")).sendKeys("test");
     driver.findElement(By.id("vB_Editor_001_save")).click();
-    driver.findElement(By.cssSelector("a > strong")).click();
+    new Select(driver.findElement(By.name("folderid"))).selectByVisibleText("Sent Items (1 Messages)");
+    driver.findElement(By.cssSelector("td > input.button")).click();
+    driver.findElement(By.linkText("testing123")).click();
     assertEquals("test", driver.findElement(By.id("post_message_")).getText());
-    driver.findElement(By.xpath("(//a[contains(text(),'Private Messages')])[2]")).click();
+    driver.findElement(By.linkText("Sent Items")).click();
+    driver.findElement(By.id("checkall_all")).click();
+    driver.findElement(By.id("pm_inlinego")).click();
+    new Select(driver.findElement(By.name("folderid"))).selectByVisibleText("Inbox (1 Messages)");
     driver.findElement(By.id("checkall_all")).click();
     new Select(driver.findElement(By.name("dowhat"))).selectByVisibleText("Delete");
-    driver.findElement(By.id("pm_inlinego")).click();
-    new Select(driver.findElement(By.name("folderid"))).selectByVisibleText("Sent Items (1 Messages)");
-    driver.findElement(By.id("checkall_all")).click();
     driver.findElement(By.id("pm_inlinego")).click();
   }
 
