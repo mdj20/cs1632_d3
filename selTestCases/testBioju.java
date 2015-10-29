51002f9c03ddda64cc58c767f56fbd0868a4a34c
Matthew <mdj20@pitt.edu>
2015-10-29 06:36:21 -0400

    Finished %80 cases

---
+++ selTestCases/testBioju.java
@@ -0,0 +1,81 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestBio {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.myth-weavers.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testBio() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("Log In")).click();
+    driver.findElement(By.id("navbar_username")).clear();
+    driver.findElement(By.id("navbar_username")).sendKeys("1n50mniac");
+    driver.findElement(By.id("navbar_password")).clear();
+    driver.findElement(By.id("navbar_password")).sendKeys("passpass");
+    driver.findElement(By.cssSelector("input.button")).click();
+    driver.findElement(By.linkText("Click here if your browser does not automatically redirect you.")).click();
+    driver.findElement(By.linkText("1n50mniac")).click();
+    driver.findElement(By.id("aboutme_tab")).click();
+    assertEquals("I was born to eat hamburgers.", driver.findElement(By.id("profilefield_value_1")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ selTestCases/testGenderju.java
@@ -0,0 +1,82 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestGender {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.myth-weavers.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testGender() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("Log In")).click();
+    driver.findElement(By.id("navbar_username")).clear();
+    driver.findElement(By.id("navbar_username")).sendKeys("1n50mniac");
+    driver.findElement(By.id("navbar_password")).clear();
+    driver.findElement(By.id("navbar_password")).sendKeys("passpass");
+    driver.findElement(By.cssSelector("input.button")).click();
+    driver.findElement(By.linkText("Click here if your browser does not automatically redirect you.")).click();
+    driver.findElement(By.linkText("1n50mniac")).click();
+    driver.findElement(By.id("stats_tab")).click();
+    driver.findElement(By.id("aboutme_tab")).click();
+    assertEquals("Male", driver.findElement(By.id("profilefield_value_5")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
